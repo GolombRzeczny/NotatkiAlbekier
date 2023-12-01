8187572a74d253704cb82409ef24b6f85e6f5203
GolombRzeczny <gabriel.albekier@gmail.com>
2023-12-01 10:37:41 +0100

    Initial commit
    Dodanie Notatek do repozytorium

---
+++ Biologia/Dział10.cpp
@@ -0,0 +1,31 @@
+Biologia
+
+  Temat: Budowa i działanie narządu wzroku
+    
+    1. Receptory:
+      
+      - Eksteroreceptory(Bodźce z zewnątrz)
+      - Interoreceptory(Bodźce z wewnątrz)
+    2. Rodzaje:
+      
+      - Fotoreceptory
+      - Termoreceptory
+      - Mechanoreceptory
+      - Chemoreceptory
+      - Nocyreceptory
+
+    3. Budowa Oka:
+      
+      - Rogówka
+      - Komory Oka
+      - Spojówka
+      - Ciało szkliste 
+      - Tęczówka
+      - Źrenica
+      - Soczewka
+      - Nerw Wzrokowy
+      - Plamka
+      - Siatkówka
+      - Twardówka
+      - Naczyniówka
+
